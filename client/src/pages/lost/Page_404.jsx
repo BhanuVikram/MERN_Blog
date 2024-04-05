0bef2e8797a12bc8f66bcdcddce17983c10e0df8
bhanuvikram <5193270+bhanuvikram@users.noreply.github.com>
2024-04-05 10:13:33 -0700

    Add: Lost pages

---
+++ client/src/pages/lost/Page_404.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page_404 = () => {
+  return <div>Page_404</div>;
+};
+
+export default Page_404;
---
+++ client/src/pages/lost/Page_500.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page_500 = () => {
+  return <div>Page_500</div>;
+};
+
+export default Page_500;
