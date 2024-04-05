aedca1580a03497ac054f013a07728d72ce227d4
bhanuvikram <5193270+bhanuvikram@users.noreply.github.com>
2024-04-05 10:11:38 -0700

    Feature: Integrated layouts

---
+++ client/src/layout/Footer.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const Footer = () => {
+  return <div>Footer</div>;
+};
+
+export default Footer;
---
+++ client/src/layout/Header.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
