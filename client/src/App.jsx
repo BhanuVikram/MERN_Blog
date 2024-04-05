4efbfaa4b99f17b13a277c23ae2a901209a951e4
bhanuvikram <5193270+bhanuvikram@users.noreply.github.com>
2024-04-05 10:09:47 -0700

    Feature: client routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+
+// * ROUTING MODULES
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// * PAGES IMPORT
+import Home from "./pages/user/Home";
+import Blog from "./pages/user/Blog";
+import Dashboard from "./pages/admin/Dashboard";
+import SignIn from "./pages/auth/SignIn";
+import SignUp from "./pages/auth/SignUp";
+import Page_404 from "./pages/lost/Page_404";
+import Page_500 from "./pages/lost/Page_500";
+
+// * LAYOUTS IMPORT
+import MainLayout from "./layout/MainLayout";
+import AuthLayout from "./layout/AuthLayout";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <MainLayout>
+              <Home />
+            </MainLayout>
+          }
+        />
+
+        <Route
+          path="/blog/:_id"
+          element={
+            <MainLayout>
+              <Blog />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <MainLayout>
+              <Dashboard />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/signin"
+          element={
+            <AuthLayout>
+              <SignIn />
+            </AuthLayout>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <AuthLayout>
+              <SignUp />
+            </AuthLayout>
+          }
+        />
+        <Route path="/404" element={<Page_404 />} />
+        <Route path="/500" element={<Page_500 />} />
+        <Route path="*" element={<Navigate to="/404" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
