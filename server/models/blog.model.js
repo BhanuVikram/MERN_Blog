db275d820b819606298a3f4010195f4b54c71658
bhanuvikram <5193270+bhanuvikram@users.noreply.github.com>
2024-04-07 13:02:42 -0700

    Feature: Created blog model

---
+++ server/models/blog.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    minlength: [4, "Title cannot be shorter than four characters"],
+    maxlength: [64, "Title cannot be longer than 64 characters"],
+    required: [true, "Please enter a title"],
+  },
+  content: {
+    type: String,
+    minlength: [128, "Content cannot be shorter than 128 characters"],
+    maxlength: [32768, "Content cannot be longer than 32768 characters"],
+    required: [true, "Please enter your blog content"],
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
