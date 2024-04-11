decc55f2998ff2d5ebb2b23527b46aef40cc58eb
bhanuvikram <5193270+bhanuvikram@users.noreply.github.com>
2024-04-11 12:49:40 -0700

    Feature: Header and its styling

--- client/src/layouts/Header.jsx
+++ client/src/layouts/Header.jsx
@@ -1,10 +1,17 @@
 import React, { useState, useEffect, useRef } from "react";
 import "../styles/layoutsStyles/headerStyles.scss";
+import logo from "../assets/images/MERN_Blog_Logo.png";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="header">
+      <img src={logo} alt="MERN Blog Logo" />
+      <div className="nav-bar">
+        <button>Sign In</button>
+        <button>Sign Up</button>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
