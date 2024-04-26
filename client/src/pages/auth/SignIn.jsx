bf9483706b3217c357ed9c2db01099152b63cb52
bhanuvikram <5193270+bhanuvikram@users.noreply.github.com>
2024-04-25 22:49:46 -0700

    Feature: Auth pages styles

--- client/src/pages/auth/SignIn.jsx
+++ client/src/pages/auth/SignIn.jsx
@@ -2,61 +2,71 @@ import React from "react";
 import * as Yup from "yup";
 import { Formik, Field, ErrorMessage, Form } from "formik";
 import axios from "axios";
-import "../../styles/pagesStyles/authPagesStyles/signInPageStyles.scss";
+import "../../styles/pagesStyles/authPagesStyles/authPageStyles.scss";
 
 const SignInSchema = Yup.object().shape({
-  username: Yup.string().required("Username is required"),
-  password: Yup.string()
-    .min(8, "Password must be at least 8 characters")
-    .required("Password is required"),
+  username: Yup.string().required("* Username is required"),
+  password: Yup.string().required("* Valid password is required"),
 });
 
 const SignIn = () => {
   return (
-    <div>
-      <Formik
-        initialValues={{
-          username: "",
-          password: "",
-        }}
-        validationSchema={SignInSchema}
-        onSubmit={(values) => {
-          console.log(values);
-          axios
-            .post(`http://localhost:8000/api/v1/signin`, values)
-            .then((res) => {
-              console.log(res.message);
-              localStorage.setItem("accessToken", res.data.token);
-            })
-            .catch((err) => {
-              console.log(res.error);
-            });
-        }}
-      >
-        <Form>
-          <label htmlFor="username">Username:</label>
-          <Field
-            type="username"
-            name="username"
-            id="username"
-            placeholder="janedoe"
-            autoComplete="off"
-          />
-          <ErrorMessage name="username" component={"div"} />
+    <div className="auth-form-container">
+      <div className="auth-form">
+        <Formik
+          initialValues={{
+            username: "",
+            password: "",
+          }}
+          validationSchema={SignInSchema}
+          onSubmit={(values) => {
+            console.log(values);
+            axios
+              .post(`http://localhost:8000/api/v1/signin`, values)
+              .then((res) => {
+                console.log(res.message);
+                localStorage.setItem("accessToken", res.data.token);
+              })
+              .catch((err) => {
+                console.log(res.error);
+              });
+          }}
+        >
+          <Form>
+            <label htmlFor="username">Username</label>
+            <Field
+              type="username"
+              name="username"
+              id="username"
+              className="field"
+              placeholder="janedoe"
+              autoComplete="off"
+            />
+            <ErrorMessage
+              name="username"
+              component={"div"}
+              className="error-message"
+            />
 
-          <label htmlFor="password">Password:</label>
-          <Field
-            type="password"
-            name="password"
-            id="password"
-            placeholder="********"
-            autoComplete="off"
-          />
-          <ErrorMessage name="password" component={"div"} />
+            <label htmlFor="password">Password</label>
+            <Field
+              type="password"
+              name="password"
+              id="password"
+              className="field"
+              placeholder="********"
+              autoComplete="off"
+            />
+            <ErrorMessage
+              name="password"
+              component={"div"}
+              className="error-message"
+            />
 
-          <button type="submit">Sign In</button>
-        </Form>
-      </Formik>
+            <button type="submit">Sign In</button>
+          </Form>
+        </Formik>
+      </div>
     </div>
   );
 };
--- client/src/pages/auth/SignUp.jsx
+++ client/src/pages/auth/SignUp.jsx
@@ -2,98 +2,105 @@ import React from "react";
 import * as Yup from "yup";
 import { Formik, Field, ErrorMessage, Form } from "formik";
 import axios from "axios";
-import "../../styles/pagesStyles/authPagesStyles/signUpPageStyles.scss";
+import "../../styles/pagesStyles/authPagesStyles/authPageStyles.scss";
 
 const SignUpSchema = Yup.object().shape({
-  firstname: Yup.string().required("Enter your first name"),
-  lastname: Yup.string().required("Enter your last name"),
-  username: Yup.string().required("Enter your desired username"),
+  firstname: Yup.string().required("* First name is required"),
+  lastname: Yup.string().required("* Last name is required"),
+  username: Yup.string().required("* Username is required"),
   email: Yup.string()
-    .email("Enter a valid email address")
-    .required("Email is required"),
+    .email("* Enter a valid email address")
+    .required("* Email is required"),
   password: Yup.string()
-    .min(8, "Password must be at least 8 characters")
-    .required("Password is required"),
+    .min(8, "* Password must be at least 8 characters")
+    .required("* Password is required"),
 });
 
 const SignUp = () => {
   return (
-    <div>
-      <Formik
-        initialValues={{
-          firstname: "",
-          lastname: "",
-          username: "",
-          email: "",
-          password: "",
-        }}
-        validationSchema={SignUpSchema}
-        onSubmit={(values) => {
-          console.log("values:", values);
-          axios
-            .post(`http://localhost:8000/api/v1/signup`, values)
-            .then((res) => {
-              console.log(res.message);
-            })
-            .catch((err) => {
-              console.log(res.error);
-            });
-        }}
-      >
-        <Form>
-          <label htmlFor="firstname">First Name:</label>
-          <Field
-            type="text"
-            name="firstname"
-            id="firstname"
-            placeholder="John"
-            autoComplete="off"
-          />
-          <ErrorMessage name="firstname" component={"div"} />
+    <div className="auth-form-container">
+      <div className="auth-form">
+        <Formik
+          initialValues={{
+            firstname: "",
+            lastname: "",
+            username: "",
+            email: "",
+            password: "",
+          }}
+          validationSchema={SignUpSchema}
+          onSubmit={(values) => {
+            console.log("values:", values);
+            axios
+              .post(`http://localhost:8000/api/v1/signup`, values)
+              .then((res) => {
+                console.log(res.message);
+              })
+              .catch((err) => {
+                console.log(res.error);
+              });
+          }}
+        >
+          <Form>
+            <label htmlFor="firstname">First Name</label>
+            <Field
+              type="text"
+              name="firstname"
+              id="firstname"
+              className="field"
+              placeholder="John"
+              autoComplete="off"
+            />
+            <ErrorMessage name="firstname" component={"div"} className="error-message"/>
 
-          <label htmlFor="lastname">Last Name:</label>
-          <Field
-            type="text"
-            name="lastname"
-            id="lastname"
-            placeholder="Doe"
-            autoComplete="off"
-          />
-          <ErrorMessage name="lastname" component={"div"} />
+            <label htmlFor="lastname">Last Name</label>
+            <Field
+              type="text"
+              name="lastname"
+              id="lastname"
+              className="field"
+              placeholder="Doe"
+              autoComplete="off"
+            />
+            <ErrorMessage name="lastname" component={"div"} className="error-message"/>
 
-          <label htmlFor="username">Username:</label>
-          <Field
-            type="text"
-            name="username"
-            id="username"
-            placeholder="johndoe"
-            autoComplete="off"
-          />
-          <ErrorMessage name="username" component={"div"} />
+            <label htmlFor="username">Username</label>
+            <Field
+              type="text"
+              name="username"
+              id="username"
+              className="field"
+              placeholder="johndoe"
+              autoComplete="off"
+            />
+            <ErrorMessage name="username" component={"div"} className="error-message"/>
 
-          <label htmlFor="email">Email:</label>
-          <Field
-            type="text"
-            name="email"
-            id="email"
-            placeholder="abc@xyz.com"
-            autoComplete="off"
-          />
-          <ErrorMessage name="email" component={"div"} />
+            <label htmlFor="email">Email</label>
+            <Field
+              type="text"
+              name="email"
+              id="email"
+              className="field"
+              placeholder="abc@xyz.com"
+              autoComplete="off"
+            />
+            <ErrorMessage name="email" component={"div"} className="error-message"/>
 
-          <label htmlFor="password">Password:</label>
-          <Field
-            type="password"
-            name="password"
-            id="password"
-            placeholder="********"
-            autoComplete="off"
-          />
-          <ErrorMessage name="password" component={"div"} />
+            <label htmlFor="password">Password</label>
+            <Field
+              type="password"
+              name="password"
+              id="password"
+              className="field"
+              placeholder="********"
+              autoComplete="off"
+            />
+            <ErrorMessage name="password" component={"div"} className="error-message"/>
 
-          <button type="submit">Sign Up</button>
-        </Form>
-      </Formik>
+            <button type="submit">Sign Up</button>
+          </Form>
+        </Formik>
+      </div>
     </div>
   );
 };
