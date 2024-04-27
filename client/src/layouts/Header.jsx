643978c5bfd4828d956438d0ec3636477df820f0
bhanuvikram <5193270+bhanuvikram@users.noreply.github.com>
2024-04-27 09:42:16 -0700

    Feature: Auth conditional rendering and sign out button on hover

--- client/src/layouts/Header.jsx
+++ client/src/layouts/Header.jsx
@@ -1,17 +1,63 @@
 import React, { useState, useEffect, useRef } from "react";
 import "../styles/layoutsStyles/headerStyles.scss";
 import logo from "../assets/images/MERN_Blog_Logo.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <div className="header">
-      <img src={logo} alt="MERN Blog Logo" />
-      <div className="nav-bar">
-        <button>Sign In</button>
-        <button>Sign Up</button>
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+
+  const token = localStorage.getItem("accessToken");
+  const username = localStorage.getItem("username");
+
+  useEffect(() => {
+    if (token && username) {
+      setUserLoggedIn(true);
+    }
+  }, [token, username]);
+
+  if (userLoggedIn) {
+    return (
+      <div className="header">
+        <Link to={`/`}>
+          <img src={logo} alt="MERN Blog Logo" />
+        </Link>
+
+        <div className="nav-bar username">
+          <div class="dropdown">
+            <button class="dropbtn">{username}</button>
+            <div class="dropdown-content">
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  localStorage.removeItem("accessToken");
+                  localStorage.removeItem("username");
+                  window.location.reload();
+                }}
+              >
+                Sign Out
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="header">
+        <Link to={`/`}>
+          <img src={logo} alt="MERN Blog Logo" />
+        </Link>
+        <div className="nav-bar">
+          <Link to={`/signin`}>
+            <button className="auth-buttons">Sign In</button>
+          </Link>
+          <Link to={`/signup`}>
+            <button className="auth-buttons sign-up-button">Sign Up</button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Header;
